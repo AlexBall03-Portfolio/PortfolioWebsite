a0640ef2d8fab9ab554cdf709d56fbc6bbd5a566
AlexBall03 <alexanderdball03@gmail.com>
2022-08-25 09:17:49 -0700

    Reset the project. I am going to restart and only focus on one aspect of the website at a time. First, I need to right out the complete structure, then I'll get to the styling and functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Styles/Global.css';
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
