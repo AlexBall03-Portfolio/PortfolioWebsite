465b1b883ceefea9b556717a56aac60b2f2074ae
AlexBall03 <alexanderdball03@gmail.com>
2022-08-29 15:57:44 -0700

    Made toggle navigation functional. Also, styled the <Nav /> a little bit more (still have more to go though)

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,11 +2,18 @@ import { FaRegEnvelope } from 'react-icons/fa';
 import { AiOutlineHome, AiOutlineInfoCircle } from 'react-icons/ai';
 import { BsHddStack, BsCodeSlash } from 'react-icons/bs';
 import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
 import "../Styles/Nav.css";
  
 function Nav() {
-    const sectionTitle = "<Home>";
+    const [navToggled, setNavToggled] = useState(false)
+
+    const ToggleNavbar = () => {
+        navToggled ? setNavToggled(false) : setNavToggled(true);
+    }
     
+    const sectionTitle = "<Home>";
+
     return (
         <nav className="Nav">
             <div className="contact-btn_Container">
@@ -20,12 +27,12 @@ function Nav() {
             </div>
 
             <div className='nav-toggle_Container'>
-                <button>
+                <button onClick={ToggleNavbar}>
                     <FaBars />
                 </button>
             </div>
 
-            <ul className='nav_Container'>
+        <ul className={`nav_Container ${navToggled ? "show-nav" : ""}`}>
                 <li><a href="#home" className='internal-link'><AiOutlineHome className='icon' /> Home</a></li>
                 <li><a href="#home" className='internal-link'><AiOutlineInfoCircle className='icon' /> About Me</a></li>
                 <li><a href="#home" className='internal-link'><BsHddStack className='icon' /> My Skills</a></li>
