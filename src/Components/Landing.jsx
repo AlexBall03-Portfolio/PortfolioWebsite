9f6ba82caf21c5a37552173551e08f4cd92ad9eb
AlexBall03 <alexanderdball03@gmail.com>
2022-08-31 14:21:50 -0700

    Cleaned up some of the styles and edited some other stuff...

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,9 +1,18 @@
+import Banner from "../Assets/Blue-Dark_Keyboard_Background-img.jpg";
+
 function Landing() {
     return (
         <div className="Landing">
             <div className="landing_Container">
-                <h2>Alex Ball</h2>
-                <h3>Frontend Web Developer</h3>
+                <div className="banner">
+                    <img src={Banner} alt="" />
+                    <div className="img-gradient"></div>
+                </div>
+                
+                <div className="greeting">
+                    <h2>Alex Ball</h2>
+                    <h3>Frontend Web Developer</h3>
+                </div>
             </div>
         </div>
     );
