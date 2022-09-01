bd045951c358c05d75585523c7590475e65c801d
AlexBall03 <alexanderdball03@gmail.com>
2022-09-01 10:45:30 -0700

    Styled <Skills /> component

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -2,17 +2,25 @@ import { AiOutlineHtml5 } from 'react-icons/ai';
 import { RiCss3Line } from 'react-icons/ri';
 import { SiJavascript, SiReact } from 'react-icons/si';
 import { BsHddStack } from 'react-icons/bs';
+import "../Styles/Skills.css";
 
 function Skills() {
     return (
         <div className="Skills">
-            <h2><BsHddStack className='icon' /> | My Skills</h2>
-            <ul className="skill-icons_Container">
-                <li><AiOutlineHtml5 className='icon html_Icon' /></li>
-                <li><RiCss3Line className='icon css_Icon' /></li>
-                <li><SiJavascript className='icon js_Icon' /></li>
-                <li><SiReact className='icon react_Icon' /></li>
-            </ul>
+            <div className="skills_Container">
+                <header className='section_Header'>
+                    <h2><BsHddStack className='icon' /> | My Skills</h2>
+                </header>
+
+                <main className='section_Main'>
+                    <ul className="skill-icons_Container">
+                        <li><AiOutlineHtml5 className='icon html_Icon' /></li>
+                        <li><RiCss3Line className='icon css_Icon' /></li>
+                        <li><SiJavascript className='icon js_Icon' /></li>
+                        <li><SiReact className='icon react_Icon' /></li>
+                    </ul>
+                </main>
+            </div>
         </div>
     );
 }
