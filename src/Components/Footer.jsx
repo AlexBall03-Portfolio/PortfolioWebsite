d4771b2b15ebd4e36827b3bf3a53aba9b21a8960
AlexBall03 <alexanderdball03@gmail.com>
2022-09-02 16:15:52 -0700

    Styled <Footer />

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,30 @@
 import { AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai';
+import "../Styles/Footer.css";
 
 function Footer() {
     const logo = "</Alex-Ball\\>"
-    
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="Footer">
-            <a href="/"><h2>{logo}</h2></a>
-            <h3>Developer <span>Portfolio</span></h3>
-            <p>Made by <span>{logo}</span> to show my work as a Developer.</p>
-            <ul className="social-links_Container">
-                <li><a href="https://www.linkedin.com/in/alexball03/"><AiOutlineLinkedin className='icon' />|LinkedIn</a></li>
-                <li><a href="https://github.com/AlexBall03"><AiOutlineGithub className='icon' />|GitHub</a></li>
-            </ul>
+            <div className="footer_Container">
+                <section className='heading_Section'>
+                    <h2>{logo}</h2>
+                    <h3>Developer <span>Portfolio</span></h3>
+                </section>
+
+                <section className='site-developer_Section'>
+                    <p>Developer: Alex Ball</p>
+                    <p>2022 - {currentYear}</p>
+                </section>
+
+                <section className='social-links_Section'>
+                    <ul className="social-links_Container">
+                        <li><a href="https://www.linkedin.com/in/alexball03/" target="_black"><AiOutlineLinkedin className='icon' />|LinkedIn</a></li>
+                        <li><a href="https://github.com/AlexBall03" target="_blank"><AiOutlineGithub className='icon' />|GitHub</a></li>
+                    </ul>
+                </section>
+            </div>
         </footer>
     );
 }
