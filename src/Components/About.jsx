c3d9d7c00cce611a7d7f310aaf833aa325a4e9b1
AlexBall03 <alexanderdball03@gmail.com>
2022-09-03 14:01:54 -0700

    Fixed a period in <About /> component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,7 +15,7 @@ function About() {
                 <main className='section_Main'>
                     <p>My name is Alex, and I love programming. I am very much a perfectionist and have a decent amount of attention to detail, so I like things to be as perfect as possible both within the UI/UX and the code, or it will drive me insane!</p>
                     <br />
-                    <p>My current Tech Stack is: HTML, CSS, JS, React.js and Git/GitHub 
+                    <p>My current Tech Stack is: HTML, CSS, JS, React.js and Git/GitHub.
                     I also know a very basic amount of C/C++ and C#/.Net (along with the very basics of how compilers work).</p>
                     <br />
                     <p>I love learning new things and am willing to learn whatever is needed to get the job done!</p> 
