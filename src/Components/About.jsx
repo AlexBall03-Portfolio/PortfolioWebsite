40e908744c2be2860b4627bf7e5cce93efb15cb1
AlexBall03 <alexanderdball03@gmail.com>
2022-09-03 18:05:40 -0700

    Edited scrollbar and back to top button

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -13,10 +13,10 @@ function About() {
                     <img src={ProfileImage} alt="Alex Ball" />
                 </div>
                 <main className='section_Main'>
-                    <p>My name is Alex, and I love programming. I am very much a perfectionist and have a decent amount of attention to detail, so I like things to be as perfect as possible both within the UI/UX and the code, or it will drive me insane!</p>
+                    <p>My name is Alex, and I love programming. I am very much a perfectionist and ___ attention to detail, so I like things to be as perfect as possible both within the UI/UX and the code, or it will drive me insane!</p>
                     <br />
                     <p>My current Tech Stack is: HTML, CSS, JS, React.js and Git/GitHub.
-                    I also know a very basic amount of C/C++ and C#/.Net (along with the very basics of how compilers work).</p>
+                    I also know a basic amount of C/C++ and C#/.Net along with the basics of how compilers work.</p>
                     <br />
                     <p>I love learning new things and am willing to learn whatever is needed to get the job done!</p> 
                 </main>
