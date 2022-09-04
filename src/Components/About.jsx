0ce142e207440790164f0bf60a0aa81ac7202dc9
AlexBall03 <alexanderdball03@gmail.com>
2022-09-03 20:52:39 -0700

    Edited <About /> component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -13,7 +13,7 @@ function About() {
                     <img src={ProfileImage} alt="Alex Ball" />
                 </div>
                 <main className='section_Main'>
-                    <p>My name is Alex, and I love programming. I am very much a perfectionist and ___ attention to detail, so I like things to be as perfect as possible both within the UI/UX and the code, or it will drive me insane!</p>
+                    <p>My name is Alex, and I love programming. I am very much a perfectionist and have a lot of attention to detail, so I like things to be as perfect as possible both within the UI/UX and the code, or it will drive me insane!</p>
                     <br />
                     <p>My current Tech Stack is: HTML, CSS, JS, React.js and Git/GitHub.
                     I also know a basic amount of C/C++ and C#/.Net along with the basics of how compilers work.</p>
