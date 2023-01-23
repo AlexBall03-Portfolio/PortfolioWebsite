5d77e12615ba59a352b9e3f0fd92eab62f18e8e6
AlexBall03 <alexanderdball03@gmail.com>
2023-01-23 15:38:04 -0700

    Updated email. Contact form currently not working

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,60 +1,85 @@
-import { FaRegEnvelope, FaRegUser, FaRegComments } from 'react-icons/fa';
-import { GiPaperPlane } from 'react-icons/gi';
-import "../Styles/Contact.css"
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import { FaRegEnvelope, FaRegUser, FaRegComments } from "react-icons/fa";
+import { GiPaperPlane } from "react-icons/gi";
+import "../Styles/Contact.css";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
-    // EmailJS Public API Key
-    const EmailJs_Public_API_Key = process.env.REACT_APP_EMAILJS_PUBLIC_API_KEY;
+  // EmailJS Public API Key
+  const EmailJs_Public_API_Key = process.env.REACT_APP_EMAILJS_PUBLIC_API_KEY;
 
-    // EmailJS Form Functionality
-    const form = useRef();
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_2de801c', 'template_ir7tp6n', form.current, EmailJs_Public_API_Key)
-        .then((result) => {
-            console.log(result.text);
-            console.log("Message sent!");
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset();
-    };
+  // EmailJS Form Functionality
+  const form = useRef();
 
-    // JSX Template
-    return (
-        <div name='contact' className="Contact">
-            <div className="contact_Container">
-                <section className='heading_Section'>
-                    <h2><FaRegEnvelope className='icon' /> | Contact Me | <GiPaperPlane /> </h2>
-                </section>
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-                <section className='form_Section'>
-                    <form ref={form} onSubmit={sendEmail} className='contact_Form'>
-                        <div className='input-group'>
-                            <input type="text" name='subject' required />
-                            <label htmlFor="subject"><FaRegUser /> Subject*</label>
-                        </div>
-                        <div className='input-group'>
-                            <input type="text" name='sender_Name' required />
-                            <label htmlFor="name"><FaRegUser /> Your Name*</label>
-                        </div>
-                        <div className='input-group'>
-                            <input type="email" name='sender_Email' required />
-                            <label htmlFor="email"><FaRegEnvelope /> Your Email*</label>
-                        </div>
-                        <div className='input-group'>
-                            <textarea name="message" rows="8" required></textarea>
-                            <label htmlFor="message"><FaRegComments /> Your Message*</label>
-                        </div>
-                        <button type='submit'>Send <GiPaperPlane /></button>
-                    </form>
-                </section>
+    emailjs
+      .sendForm(
+        "service_s2639to",
+        "template_njbp11c",
+        form.current,
+        EmailJs_Public_API_Key
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Email sent!");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Email failed to send...");
+        }
+      );
+    e.target.reset();
+  };
+
+  // JSX Template
+  return (
+    <div name="contact" className="Contact">
+      <div className="contact_Container">
+        <section className="heading_Section">
+          <h2>
+            <FaRegEnvelope className="icon" /> | Contact Me | <GiPaperPlane />{" "}
+          </h2>
+          <br />
+          <p>NOTE: Form not working</p>
+        </section>
+
+        <section className="form_Section">
+          <form ref={form} onSubmit={sendEmail} className="contact_Form">
+            <div className="input-group">
+              <input type="text" name="subject" required />
+              <label htmlFor="subject">
+                <FaRegUser /> Subject*
+              </label>
             </div>
-        </div>
-    );
+            <div className="input-group">
+              <input type="text" name="sender_Name" required />
+              <label htmlFor="name">
+                <FaRegUser /> Your Name*
+              </label>
+            </div>
+            <div className="input-group">
+              <input type="email" name="sender_Email" required />
+              <label htmlFor="email">
+                <FaRegEnvelope /> Your Email*
+              </label>
+            </div>
+            <div className="input-group">
+              <textarea name="message" rows="8" required></textarea>
+              <label htmlFor="message">
+                <FaRegComments /> Your Message*
+              </label>
+            </div>
+            <button type="submit">
+              Send <GiPaperPlane />
+            </button>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,37 +1,75 @@
 import Resume from "../Assets/MyResume.pdf";
-import { AiOutlineLinkedin, AiOutlineGithub, AiOutlineDownload } from 'react-icons/ai';
+import {
+  AiOutlineLinkedin,
+  AiOutlineGithub,
+  AiOutlineDownload,
+} from "react-icons/ai";
 import "../Styles/Footer.css";
 
 function Footer() {
-    const logo = "</Alex-Ball\\>"
-    const currentYear = new Date().getFullYear();
+  const logo = "</Alex-Ball\\>";
+  const currentYear = new Date().getFullYear();
 
-    return (
-        <footer className="Footer">
-            <div className="footer_Container">
-                <section className='heading_Section'>
-                    <h2>{logo}</h2>
-                    <h3>Developer <span>Portfolio</span></h3>
-                    <div className='email_Container'>
-                        <a href="mailto:portfolio.alexball@gmail.com" rel='noreferrer' target="_blank">portfolio.alexball@gmail.com</a>
-                    </div>
-                </section>
+  return (
+    <footer className="Footer">
+      <div className="footer_Container">
+        <section className="heading_Section">
+          <h2>{logo}</h2>
+          <h3>
+            Developer <span>Portfolio</span>
+          </h3>
+          <div className="email_Container">
+            <a
+              href="mailto:admin@alexballportfolio.dev"
+              rel="noreferrer"
+              target="_blank"
+            >
+              admin@alexballportfolio.dev
+            </a>
+          </div>
+        </section>
 
-                <section className='site-developer_Section'>
-                    <p>Developed by: Alex Ball</p>
-                    <p>2022 - {currentYear}</p>
-                </section>
+        <section className="site-developer_Section">
+          <p>Developed by: Alex Ball</p>
+          <p>2022 - {currentYear}</p>
+        </section>
 
-                <section className='links_Section'>
-                    <ul className="links_Container">
-                        <li><a href="https://www.linkedin.com/in/alexball03/" rel='noreferrer' target="_black"><AiOutlineLinkedin className='icon' /><br />|LinkedIn</a></li>
-                        <li><a href="https://github.com/AlexBall03" rel='noreferrer' target="_blank"><AiOutlineGithub className='icon' /><br />|GitHub</a></li>
-                        <li><a href={Resume} rel='noreferrer' target="_blank"><AiOutlineDownload /><br />|Resume</a></li>
-                    </ul>
-                </section>
-            </div>
-        </footer>
-    );
+        <section className="links_Section">
+          <ul className="links_Container">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/alexball03/"
+                rel="noreferrer"
+                target="_black"
+              >
+                <AiOutlineLinkedin className="icon" />
+                <br />
+                |LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/AlexBall03"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <AiOutlineGithub className="icon" />
+                <br />
+                |GitHub
+              </a>
+            </li>
+            <li>
+              <a href={Resume} rel="noreferrer" target="_blank">
+                <AiOutlineDownload />
+                <br />
+                |Resume
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
