526233da93997740796773f980994cb0eff6a6b8
AlexBall03 <alexanderdball03@gmail.com>
2023-01-23 12:33:31 -0700

    Changed profile picture and restyled the toggle button and the ToTop button

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,28 +1,41 @@
-import { AiOutlineInfoCircle } from 'react-icons/ai';
-import ProfileImage from "../Assets/MyProfilePic.jpeg";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import ProfileImage from "../Assets/ProfilePicture.jpg";
 import "../Styles/About.css";
 
 function About() {
-    return ( 
-        <div name='about' className="About">
-            <div className='about_Container'>
-                <header className='heading_Section'>
-                    <h2><AiOutlineInfoCircle className='icon' /> | About Me</h2>
-                </header>
-                <div className='profile-img_Container'>
-                    <img src={ProfileImage} alt="Alex Ball" />
-                </div>
-                <main className='section_Main'>
-                    <p>My name is Alex, and I love programming. I am very much a perfectionist and have a lot of attention to detail, so I like things to be as perfect as possible both within the UI/UX and the code, or it will drive me insane!</p>
-                    <br />
-                    <p>My current Tech Stack is: HTML5, CSS3, JavaScript, React and Git/GitHub.
-                    I also know a basic amount of C#/.Net and C/C++ along with the basics of how compilers work.</p>
-                    <br />
-                    <p>I love learning new things and am willing to learn whatever is needed to get the job done!</p> 
-                </main>
-            </div>
+  return (
+    <div name="about" className="About">
+      <div className="about_Container">
+        <header className="heading_Section">
+          <h2>
+            <AiOutlineInfoCircle className="icon" /> | About Me
+          </h2>
+        </header>
+        <div className="profile-img_Container">
+          <img src={ProfileImage} alt="Alex Ball" />
         </div>
-    );
+        <main className="section_Main">
+          <p>
+            My name is Alex, and I love programming. I am very much a
+            perfectionist and have a lot of attention to detail, so I like
+            things to be as perfect as possible both within the UI/UX and the
+            code, or it will drive me insane!
+          </p>
+          <br />
+          <p>
+            My current Tech Stack is: HTML5, CSS3, JavaScript, React and
+            Git/GitHub. I also know a basic amount of C#/.Net and C/C++ along
+            with the basics of how compilers work.
+          </p>
+          <br />
+          <p>
+            I love learning new things and am willing to learn whatever is
+            needed to get the job done!
+          </p>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,35 +1,44 @@
-import Picture from "../Assets/MyProfilePic.jpeg";
+import Picture from "../Assets/ProfilePicture.jpg";
 import { Link } from "react-scroll";
 
 function Header() {
-    // Logo
-    const myFirstName = "Alex";
-    const myLastName = "Ball";
-    const myHyphen = "-"
-    const myPreffix = "</"
-    const mySuffix = "\\>"
-    
-    return (
-        <header className="Header">
-            <section className="logo_Section">
-                <div className="logo_Container">
-                    <Link to='home' spy={true} smooth={true} duration={500} href="/">    
-                        <h1><span className="title-accent-clr">{myPreffix}</span>{myFirstName}<span className="title-accent-clr">{myHyphen}</span>{myLastName}<span className="title-accent-clr">{mySuffix}</span></h1>
-                    </Link>
-                </div>
-            </section>
-            <section className="profile-img_Section">
-                <div className="profile-img_Container">
-                    <img src={Picture} alt="Alex Ball" />
-                </div>
-            </section>
-            <section className="greeting_Section">
-                <div className="greeting_Container">
-                    <p>Hello, my name is Alex and I am a Frontend Web Developer. Welcome to my Portfolio Website!</p>
-                </div>
-            </section>
-        </header>
-    );
+  // Logo
+  const myFirstName = "Alex";
+  const myLastName = "Ball";
+  const myHyphen = "-";
+  const myPreffix = "</";
+  const mySuffix = "\\>";
+
+  return (
+    <header className="Header">
+      <section className="logo_Section">
+        <div className="logo_Container">
+          <Link to="home" spy={true} smooth={true} duration={500} href="/">
+            <h1>
+              <span className="title-accent-clr">{myPreffix}</span>
+              {myFirstName}
+              <span className="title-accent-clr">{myHyphen}</span>
+              {myLastName}
+              <span className="title-accent-clr">{mySuffix}</span>
+            </h1>
+          </Link>
+        </div>
+      </section>
+      <section className="profile-img_Section">
+        <div className="profile-img_Container">
+          <img src={Picture} alt="Alex Ball" />
+        </div>
+      </section>
+      <section className="greeting_Section">
+        <div className="greeting_Container">
+          <p>
+            Hello, my name is Alex and I am a Frontend Web Developer. Welcome to
+            my Portfolio Website!
+          </p>
+        </div>
+      </section>
+    </header>
+  );
 }
 
 export default Header;
